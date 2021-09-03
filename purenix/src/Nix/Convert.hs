aa3d0d7304e02bf8e385381bad8effbb253f9642
Jonas Carpay <jonascarpay@gmail.com>
2021-09-03 23:44:14 +0900

    Module converter

--- purenix/src/Nix/Convert.hs
+++ purenix/src/Nix/Convert.hs
@@ -9,6 +9,7 @@ where
 import Control.Applicative
 import Control.Monad.Except
 import Control.Monad.Reader
+import Data.Map
 import Data.Text (Text)
 import qualified Data.Text as T
 import Language.PureScript (Ident (..))
@@ -20,7 +21,7 @@ import qualified Nix.Expr as N
 type Convert = ReaderT (FilePath, SourceSpan) (Either Text)
 
 convert :: Module Ann -> Either Text N.Expr
-convert m = runReaderT (module' m) (modulePath m, moduleSourceSpan m)
+convert (Module spn _comments _name path imports exports reexports _foreign decls) = runReaderT (module' imports exports reexports decls) (path, spn)
 
 throw :: Text -> Convert a
 throw err = ask >>= throwError . uncurry format
@@ -37,15 +38,17 @@ localSpan spn = local (fmap $ const spn)
 localAnn :: Ann -> Convert a -> Convert a
 localAnn (spn, _, _, _) = localSpan spn
 
-module' :: Module Ann -> Convert N.Expr
-module' md = localSpan (moduleSourceSpan md) $ throw "Not yet implemented"
+module' ::
+  [(Ann, P.ModuleName)] ->
+  [Ident] ->
+  Map P.ModuleName [Ident] ->
+  [Bind Ann] ->
+  Convert N.Expr
+module' imports exports reexports decls = pure $ N.abs "imports" _
 
 expr :: Expr Ann -> Convert N.Expr
-expr (Abs ann arg body) = local' ann $ liftA2 N.Abs (ident arg) (expr body)
+expr (Abs ann arg body) = localAnn ann $ liftA2 N.abs (ident arg) (expr body)
 expr (Literal ann lit) = localAnn ann $ literal lit
-
-local' :: Ann -> Convert (N.ExprF N.Expr) -> Convert N.Expr
-local' = undefined
 
 ident :: Ident -> Convert N.Ident
 ident (Ident i) = pure i
@@ -53,6 +56,6 @@ ident (GenIdent mname n) = pure $ maybe id mappend mname (T.pack $ show n)
 ident UnusedIdent = throw "Impossible: Encountered typechecking-only identifier"
 
 literal :: Literal (Expr Ann) -> Convert N.Expr
-literal (NumericLiteral (Left n)) = pure $ N.Expr $ N.Num n
+literal (NumericLiteral (Left n)) = pure $ N.num n
 literal (NumericLiteral (Right _)) = throw "Encountered floating-point literal"
-literal (StringLiteral str) = pure $ N.Expr $ N.String $ P.prettyPrintString str
+literal (StringLiteral str) = pure $ N.string $ P.prettyPrintString str
