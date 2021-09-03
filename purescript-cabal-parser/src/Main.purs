4d97982d1c7660100d25fa25989dcee8abc55ac8
Dennis Gosnell <cdep.illabout@gmail.com>
2021-09-03 10:36:22 +0900

    Setup spago so it tries to run purenix after outputting corefn.

--- purescript-cabal-parser/src/Main.purs
+++ purescript-cabal-parser/src/Main.purs
@@ -1,10 +1,16 @@
 module Main where
 
-import Prelude
+-- import Prelude
 
-import Effect (Effect)
-import Effect.Console (log)
+-- import Effect (Effect)
+-- import Effect.Console (log)
 
-main :: Effect Unit
-main = do
-  log "🍝"
+-- main :: Effect Unit
+-- main = do
+--   log "🍝"
+
+myId :: forall a. a -> a
+myId a = a
+
+myNum :: Int
+myNum = 3
