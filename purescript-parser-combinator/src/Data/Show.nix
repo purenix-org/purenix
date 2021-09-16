2a120c78ba43d03f78a88eaf35bdb8fec887b509
Dennis Gosnell <cdep.illabout@gmail.com>
2021-09-16 09:20:05 +0900

    Add Data.Show

---
+++ purescript-parser-combinator/src/Data/Show.nix
@@ -0,0 +1,12 @@
+
+{ showIntImpl = builtins.toString
+
+# TODO: This should remove extra zeros from the end of a number
+, showNumberImpl = builtins.toString
+
+# TODO: This should probably put single quotes around a character.
+, showCharImpl = builtins.toString
+
+# TODO: This should probably add double quotes around a String.
+, showStringImpl = builtins.toString
+}
