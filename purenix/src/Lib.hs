c57c97225601b07d8288b3e57963e8fe41aa73d6
Jonas Carpay <jonascarpay@gmail.com>
2021-09-22 12:10:59 +0900

    Copy FFI files

--- purenix/src/Lib.hs
+++ purenix/src/Lib.hs
@@ -6,6 +6,7 @@ import qualified Data.Aeson as Aeson
 import Data.Aeson.Types (parseEither)
 import qualified Data.Text as T
 import qualified Data.Text.Lazy.IO as TL
+import qualified Language.PureScript.CoreFn as P
 import Language.PureScript.CoreFn.FromJSON (moduleFromJSON)
 import Nix.Convert (convert)
 import Nix.Prelude
@@ -13,10 +14,12 @@ import Nix.Print (renderExpr)
 import qualified System.Directory as Dir
 import qualified System.Exit as Sys
 import System.FilePath ((</>))
+import qualified System.FilePath as FP
 
 defaultMain :: IO ()
 defaultMain = do
-  let moduleRoot = "../purescript-cabal-parser/output/"
+  let workdir = "../purescript-cabal-parser/"
+  let moduleRoot = workdir </> "output"
   moduleDirs <- filter (/= "cache-db.json") <$> Dir.listDirectory moduleRoot
   forM_ moduleDirs $ \rel -> do
     let dir = moduleRoot </> rel
@@ -26,3 +29,8 @@ defaultMain = do
     (_version, module') <- either Sys.die pure $ parseEither moduleFromJSON value
     nix <- either (Sys.die . T.unpack) pure $ convert module'
     TL.writeFile (dir </> "default.nix") (renderExpr nix)
+    -- Copy FFI file, if one exists
+    let foreignSrc = workdir </> FP.replaceExtension (P.modulePath module') "nix"
+        foreignTrg = dir </> "foreign.nix"
+    hasForeign <- Dir.doesFileExist foreignSrc
+    when hasForeign $ Dir.copyFile foreignSrc foreignTrg
