7c9b53422ee6b6fdfbe2bf799345548f30c622e9
Dennis Gosnell <cdep.illabout@gmail.com>
2021-09-29 10:04:04 +0900

    Fix FFI for EuclideanRing

--- purescript-prelude/src/Data/EuclideanRing.nix
+++ purescript-prelude/src/Data/EuclideanRing.nix
@@ -19,5 +19,8 @@ in
         yy = abs y;
       in mod ((mod x yy) + yy) yy;
 
-  numDiv = x: y: x / y;
+  numDiv = x: y:
+    # Add 0.0 to x to make sure that it is a float and floating division will
+    # be used, even if the user accidentally passes in two integers.
+    (x + 0.0) / y;
 }
