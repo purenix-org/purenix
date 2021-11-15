835a5801e648ab9579785316fad3732859918877
(cdep)illabout <cdep.illabout@gmail.com>
2021-11-15 10:31:49 +0900

    Change purenix to build with Nixpkgs haskell infrastructure instead of haskell.nix.

--- flake.nix
+++ flake.nix
@@ -1,67 +1,48 @@
 {
   description = "PureNix";
 
-  inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
-  inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-  outputs = { self, nixpkgs, flake-utils, haskellNix }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        overlay = self: _: {
-          hsPkgs = self.haskell-nix.project' {
-            projectFileName = "stack.yaml";
-            src = ./.;
-            compiler-nix-name = "ghc8104";
-            shell = {
-              tools = {
-                cabal = { };
-                ghcid = { };
-                haskell-language-server = { };
-                hlint = { };
-                ormolu = { };
-              };
-            };
-          };
-        };
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [
-            haskellNix.overlay
-            overlay
-          ];
-        };
-        flake = pkgs.hsPkgs.flake { };
-      in
-      flake // {
-        defaultPackage = flake.packages."purenix:exe:purenix";
+  outputs = { self, nixpkgs }:
+    let
+      # System types to support.
+      supportedSystems = [
+        "aarch64-linux"
+        "aarch64-darwin"
+        "i686-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
 
+      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+
+      # Nixpkgs instantiated for supported system types.
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ self.overlay ]; });
+    in
+    {
+      overlay = import ./nix/overlay.nix;
+
+      defaultPackage = forAllSystems (system: self.packages.${system}.purenix);
+
+      packages = forAllSystems (system: {
+        inherit (nixpkgsFor.${system}) purenix;
+      });
+
+      devShell = forAllSystems (system: self.devShells.${system}.hacking-on-purenix);
+
+      devShells = forAllSystems (system: {
         # This shell is for hacking on purenix itself.  You get GHC with a
-        # suitable package database, as well as all the development tools as
-        # defined above.  You also get purs and spago that can be used for
+        # suitable package database, as well as a bunch of common Haskell
+        # development tools.  You also get purs and spago that can be used for
         # testing out purenix.
-        devShell = flake.devShell.overrideAttrs (oldAttrs: {
-          nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [
-            pkgs.hsPkgs.hsPkgs.purescript.components.exes.purs
-            pkgs.spago
-          ];
-        });
+        hacking-on-purenix = nixpkgsFor.${system}.hacking-on-purenix-shell;
 
         # This is a shell that contains purenix, purs, and spago.  It will
         # mainly be used by the flake.nix for all our PureScript packages.
         # It can also be used by users who just want to play around with
         # purenix, but not hack on it.
-        devShells.use-purenix =
-          pkgs.stdenv.mkDerivation {
-            name = "use-purenix-shell";
-            nativeBuildInputs = [
-              pkgs.hsPkgs.hsPkgs.purescript.components.exes.purs
-              pkgs.spago
-              self.defaultPackage.${system}
-            ];
-            dontUnpack = true;
-            installPhase = "touch $out";
-          };
-      }
-    );
+        use-purenix = nixpkgsFor.${system}.use-purenix-shell;
+      });
+    };
 }
